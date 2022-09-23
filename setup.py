7458ad90ba8d64625d86b1cbf2851d8f77549331
trilok <sonu.pandey1990@gmail.com>
2022-09-23 12:56:06 +0530

    intial setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md","r",encoding="utf-8") as f:
     long_description = f.read()
 
-__version__ = "0.0.0."
+__version__ = "0.0.0"
 
 REPO_NAME = "ImgClassifierCV"
 AUTHOR_USER_NAME = "Trilokpandey"
@@ -20,9 +20,9 @@ setuptools.setup(
     long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
-        "Bug Tracker"=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
-    package_dir={"","src"},
+    package_dir={"":"src"},
     packages=setuptools.find_packages(where="src")
 
 )
